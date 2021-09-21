b6f1bbf7ec5caf58ec463d03ec4885be9d399ab6
RYRJ-KSJ <rz224@cornell.edu>
2021-09-20 22:16:26 -0400

    clock

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -4,6 +4,8 @@ import digitalio
 import board
 from PIL import Image, ImageDraw, ImageFont
 import adafruit_rgb_display.st7789 as st7789
+from datetime import datetime
+from time import strftime, sleep
 
 # Configuration for CS and DC pins (these are FeatherWing defaults on M0/M4):
 cs_pin = digitalio.DigitalInOut(board.CE0)
@@ -54,17 +56,47 @@ x = 0
 # same directory as the python script!
 # Some other nice fonts to try: http://www.dafont.com/bitmap.php
 font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 18)
+font_small = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 15)
 
+#image reformation
+def image_reform(image1, width, height):
+    image1 = image1.convert('RGB')
+    image1 = image1.resize((240, 135), Image.BICUBIC)
+
+    return image1
 # Turn on the backlight
 backlight = digitalio.DigitalInOut(board.D22)
 backlight.switch_to_output()
 backlight.value = True
+
+buttonA = digitalio.DigitalInOut(board.D23)
+buttonB = digitalio.DigitalInOut(board.D24)
+buttonA.switch_to_input()
+buttonB.switch_to_input()
 
 while True:
     # Draw a black filled box to clear the image.
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
 
     #TODO: Lab 2 part D work should be filled in here. You should be able to look in cli_clock.py and stats.py 
+    x, y = 6, 8
+    if buttonA.value and buttonB.value:  # without any button pressed
+        image1 = Image.open("dali.jpg")
+        image1 = image_reform(image1, width, height)
+
+        draw = ImageDraw.Draw(image1)
+
+        draw.text((x, y), "Dali Clock", font=font, fill="#000000")
+
+
+    if buttonB.value and not buttonA.value:  # press button A
+        draw.text((x, y), "Time Stopped", font=font, fill="#000000")
+
+
+    if buttonA.value and not buttonB.value:  # press button B
+
+        draw.text((4, 0), "Time Flies", font=font1, fill="#FFFF00")
+        draw.text((x, y), strftime("%H:%M:%S%p"), font=font, fill="#000000")
 
     # Display image.
     disp.image(image, rotation)
