45052782096a899fd6c3b3eaba077a99ce18ce58
RYRJ-KSJ <rz224@cornell.edu>
2021-10-21 10:16:49 -0400

    prototype

--- Lab 4/acupuncture.py
+++ Lab 4/acupuncture.py
@@ -14,14 +14,14 @@ while True:
     for i in range(12):
         if mpr121[i].value:
             if i == 5 or i == 6:
-        	    playsound('great.mp3')
-        	elif i == 4 or i == 7:
-        		playsound('wonderful.mp3')
-        	elif i == 3 or i == 8:
-        		playsound('tryanotherpoint.mp3')
-        	elif i == 2 or i == 9:
-        		playsound('wonderful.mp3')
-        	elif i == 1 or i == 10:
-        		playsound('tryanotherpoint.mp3')
+                playsound('great.mp3')
+            elif i == 4 or i == 7:
+                playsound('wonderful.mp3')
+            elif i == 3 or i == 8:
+                playsound('tryanotherpoint.mp3')
+            elif i == 2 or i == 9:
+                playsound('wonderful.mp3')
+            elif i == 1 or i == 10:
+                playsound('tryanotherpoint.mp3')
 
     time.sleep(0.25)
